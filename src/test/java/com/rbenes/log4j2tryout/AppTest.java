940820bc985808a2217d64719a3e9ad3f7d51a96
Richard Beneš <richard.benes.9@email.cz>
2025-11-05 12:42:38 +0100

    Now testing works well but release fails

--- src/test/java/com/rbenes/log4j2tryout/AppTest.java
+++ src/test/java/com/rbenes/log4j2tryout/AppTest.java
@@ -1,19 +1,22 @@
 package com.rbenes.log4j2tryout;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.apache.logging.log4j.Marker;
+import org.apache.logging.log4j.MarkerManager;
 import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest {
+import lombok.extern.log4j.Log4j2;
 
-    /**
-     * Rigorous Test :-)
-     */
+
+@Log4j2
+public class AppTest {
+    
+    Marker MARKER = MarkerManager.getMarker("AppTestMarker");
+
     @Test
     public void shouldAnswerWithTrue() {
-        assertTrue(true);
+        log.info(MARKER, "AppTest logs");
+        assertThat(1 + 1).isEqualTo(2);
     }
 }
